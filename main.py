000dc7d5cdfc51204ea895d95e216695124696e5
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-08-07 18:10:25 +0000

    finished

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ X_train = train_data.drop('class', axis=1)  # Features (all columns except the f
 X_train = X_train / 255.0
 y_train = train_data['class']   # Target (first column)
 
+X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size = 10, random_state = 2, stratify = y_train)
+
+
 # Load the testing dataset
 test_data = pd.read_csv('fashion_mnist_20bal_test.csv')
 
@@ -21,7 +24,7 @@ X_test = X_test / 255.0
 y_test = test_data['class']   # Target (first column)
 
 
-neural_net_model = MLPClassifier( hidden_layer_sizes=(8),random_state=42,tol=0.005)
+neural_net_model = MLPClassifier( hidden_layer_sizes=(20),random_state=None,tol=0.005)
 
 neural_net_model.fit(X_train, y_train)
 # Determine model architecture 
@@ -33,7 +36,7 @@ print(f"Layer sizes: {layer_size_str}")
 
 # predict the classes from the training and test sets
 y_pred_train = neural_net_model.predict(X_train)
-y_pred = neural_net_model.predict(X_test)
+y_pred = neural_net_model.predict(X_validate)
 
 # Create dictionaries to hold total and correct counts for each class
 correct_counts = defaultdict(int)
@@ -41,7 +44,7 @@ total_counts = defaultdict(int)
 overall_correct = 0
 
 # Count correct test predictions for each class
-for true, pred in zip(y_test, y_pred):
+for true, pred in zip(y_validate, y_pred):
     total_counts[true] += 1
     if true == pred:
         correct_counts[true] += 1
@@ -61,7 +64,24 @@ for class_id in sorted(total_counts.keys()):
     accuracy = correct_counts[class_id] / total_counts[class_id] *100
     print(f"Accuracy for class {class_id}: {accuracy:3.0f}%")
 print(f"----------")
-overall_accuracy = overall_correct / len(y_test)*100
+overall_accuracy = overall_correct / len(y_validate)*100
 print(f"Overall Test Accuracy: {overall_accuracy:3.1f}%")
 overall_training_accuracy = correct_counts_training / total_counts_training*100
-print(f"Overall Training Accuracy: {overall_training_accuracy:3.1f}%")
+print(f"Overall Validation Accuracy: {overall_training_accuracy:3.1f}%")
+
+#<------------ Confusion Matrix
+conf_matrix = confusion_matrix(y_validate, y_pred)
+class_ids = sorted(total_counts.keys())
+
+# For better formatting
+print("\nConfusion Matrix:\n")
+print(f"{'':9s}", end='')
+for label in class_ids:
+    print(f"Class {label:2d} ", end='')
+print()  # Newline for next row
+
+for i, row in enumerate(conf_matrix):
+    print(f"Class {class_ids[i]}:", " ".join(f"{num:8d}" for num in row))
+
+# Misidenitfies
+print("This program often misidentifies class 4/ letter E for class 2/ letter C, but gets everything else correct")
