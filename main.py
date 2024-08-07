9d2440a2bf90f0debd4d40375b2c32063d8eaf02
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-08-07 16:43:39 +0000

    completed section

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import  confusion_matrix, classification_report
 from collections import defaultdict
+from sklearn.metrics import confusion_matrix
 
 # 8<------------ cut here ---------------
 neural_net_model = MLPClassifier( hidden_layer_sizes=(8),random_state=42,tol=0.005)
@@ -10,6 +11,10 @@ neural_net_model = MLPClassifier( hidden_layer_sizes=(8),random_state=42,tol=0.0
 # 8<------------ cut here ---------------
 # Load the training dataset
 train_data = pd.read_csv('fashion_mnist_20bal_train.csv')
+
+#Filters the train data to be in certain labels
+#is_in_labels = train_data['class'].isin([3,5,7])
+#train_data = train_data[is_in_labels]
 
 # Separate the data (features) and the  classes
 X_train = train_data.drop('class', axis=1)  # Features (all columns except the first one)
@@ -19,10 +24,24 @@ y_train = train_data['class']   # Target (first column)
 # Load the testing dataset
 test_data = pd.read_csv('fashion_mnist_20bal_test.csv')
 
+#Filters the train data to be in certain labels
+#is_in_labels = test_data['class'].isin([3,5,7])
+#test_data = test_data[is_in_labels]
+
 # Separate the data (features) and the  classes
 X_test = test_data.drop('class', axis=1)  # Features (all columns except the first one)
 X_test = X_test / 255.0
 y_test = test_data['class']   # Target (first column)
+
+
+
+# Step 1: Define the labels we are interested in
+selected_labels = [3, 7]
+# Step 2: Check if each row's label is in our list of selected labels
+is_label_selected = test_data['class'].isin(selected_labels)
+# Step 3: Filter the DataFrame based on the selected labels
+test_data = test_data[is_label_selected]
+
 
 # 8<------------ cut here ---------------
 neural_net_model.fit(X_train, y_train)
@@ -68,6 +87,17 @@ print(f"Overall Test Accuracy: {overall_accuracy:3.1f}%")
 overall_training_accuracy = correct_counts_training / total_counts_training*100
 print(f"Overall Training Accuracy: {overall_training_accuracy:3.1f}%")
 
+#<------------ Confusion Matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_ids = sorted(total_counts.keys())
 
+# For better formatting
+print("\nConfusion Matrix:\n")
+print(f"{'':9s}", end='')
+for label in class_ids:
+    print(f"Class {label:2d} ", end='')
+print()  # Newline for next row
 
+for i, row in enumerate(conf_matrix):
+    print(f"Class {class_ids[i]}:", " ".join(f"{num:8d}" for num in row))
 
