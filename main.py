006ab54f3ff757c71eaa8160c6d3d4d2fafab412
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-08-07 17:30:03 +0000

    completed section

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ X_train = train_data.drop('class', axis=1)  # Features (all columns except the f
 X_train = X_train / 255.0
 y_train = train_data['class']   # Target (first column)
 
+X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size = 20, random_state = 2, stratify = y_train)
+
 # Load the testing dataset
 test_data = pd.read_csv('fashion_mnist_20bal_test.csv')
 
@@ -35,7 +37,7 @@ print(f"Layer sizes: {layer_size_str}")
 # 8<------------ cut here ---------------
 # predict the classes from the training and test sets
 y_pred_train = neural_net_model.predict(X_train)
-y_pred = neural_net_model.predict(X_test)
+y_pred = neural_net_model.predict(X_validate)
 
 # Create dictionaries to hold total and correct counts for each class
 correct_counts = defaultdict(int)
@@ -43,7 +45,7 @@ total_counts = defaultdict(int)
 overall_correct = 0
 
 # Count correct test predictions for each class
-for true, pred in zip(y_test, y_pred):
+for true, pred in zip(y_validate, y_pred):
     total_counts[true] += 1
     if true == pred:
         correct_counts[true] += 1
@@ -63,7 +65,7 @@ for class_id in sorted(total_counts.keys()):
     accuracy = correct_counts[class_id] / total_counts[class_id] *100
     print(f"Accuracy for class {class_id}: {accuracy:3.0f}%")
 print(f"----------")
-overall_accuracy = overall_correct / len(y_test)*100
+overall_accuracy = overall_correct / len(y_validate)*100
 print(f"Overall Test Accuracy: {overall_accuracy:3.1f}%")
 overall_training_accuracy = correct_counts_training / total_counts_training*100
 print(f"Overall Training Accuracy: {overall_training_accuracy:3.1f}%")
